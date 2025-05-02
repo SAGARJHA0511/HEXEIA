f7d0e32a11bf3199a43f0979106382f9905fa5fa
Sagar Jha <sagarjha0511@gmail.com>
2025-05-02 22:58:36 +0530

    Initial stage

---
+++ HEXEIA.js
@@ -0,0 +1,18 @@
+// script.js
+
+document.getElementById("submit-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for submitting your query! We will get back to you soon.");
+    this.reset();
+  });
+
+  const slides = document.querySelectorAll('.sliderContainer > div');
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove('active');
+
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  slides[currentSlide].classList.add('active');
+}, 3000);
